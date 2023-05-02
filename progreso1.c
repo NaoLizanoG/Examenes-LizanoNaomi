a1f4838fc86041261901c58edb428d0e93b6ed3d
NaoLizano.G <naomilizano@gmail.com>
2023-05-02 12:25:43 -0500

    pleanteamiento inicial de la resolucion del problema

--- progreso1.c
+++ progreso1.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+int sumatoria(int a){
+    int resultado;
+    if (a%2==0){
+    resultado=(1)/(2*a)+1;
+    }
+    else {
+      resultado=(-1)/(2*a)+1;  
+    }
+    return resultado;
+}
 
 int main(){
+int num, b, i, sum;
+sum=0;
+printf("Ingrese el numero a evaluar\n");
+scanf("%d", &num);
 
-
+for (i=0; i<=num; i++){
+b=sumatoria(i);
+sum=sum+b;
+}
+printf("ELl resultado es: %d\n", sum);
     return 0;
 }
